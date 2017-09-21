7c7b8be9465af764ecb7f3422ba94564332c9c8c
Liang <iliang@login3.cm.cluster>
2017-09-20 23:21:09 -0700

    complete homework/hello

--- problems/hello/hello-arg.sh
+++ problems/hello/hello-arg.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $# -lt 1 ]]; then
+	printf "Usage: %s name\n" "$(basename "$0")"
+	exit 1
+fi
+
+echo "Hello, $1"!
--- problems/hello/hello-arg2.sh
+++ problems/hello/hello-arg2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $# -lt 2 ]]; then
+	printf "Usage: %s GREETING NAME\n" "$(basename "$0")"
+	exit 1
+fi
+
+GREETING=$1
+NAME=$2
+
+echo "$GREETING, $NAME"!
