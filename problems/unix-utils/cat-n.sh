955e17c8adb9371051de62d4fd9a9ec0a381aa18
Liang <iliang@login2.cm.cluster>
2017-09-28 16:01:06 -0700

    all good -make test error 127

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+
+if [[ $# -lt 1 ]]; then
+	printf "Usage: %s file\n" "$(basename "$0")"
+	exit 1
+
+elif [[ ! -f $1 ]]; then
+	printf "%s is not a file" \"$1\"
+	exit 1
+fi
+
+i=0
+FILE=$1
+while read -r LINE; do
+	let i++
+	printf "%3d %s\n" $i "$LINE"
+done < "$FILE"
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+
+if [[ $# -lt 1 ]]; then
+        printf "Usage: %s FILE [NUM]\n" "$(basename "$0")"
+        exit 1
+fi
+
+if [[ ! -f $1 ]]; then
+        printf "%s is not a file" \"$1\"
+        exit 1
+fi
+
+
+FILE=$1
+NUM=${2:-"3"}
+i=0
+
+while read -r LINE; do
+	let i++
+	if [[ $i -le $NUM ]]; then
+		printf "%s\n" "$LINE"
+	
+	elif [[ $i -gt $NUM ]]; then
+	break
+fi
+
+done < "$FILE"
