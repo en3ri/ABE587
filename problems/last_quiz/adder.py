9da14273204f767454e41a11acdd89e2ef6ab5bd
Irene Liang <iliang@email.arizona.edu>
2017-11-30 13:07:12 -0700

    adding last-quiz

--- problems/last_quiz/adder.py
+++ problems/last_quiz/adder.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
-print('OK')
+import sys
+import os
+
+args = sys.argv[1:]
+
+if len(args) != 2:
+        print('Usage: {} arg1 arg2'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
+
+arg1 = args[0]
+arg2 = args[1]
+
+if args[0].isdigit(): 
+    if args[1].isdigit():
+        print(args[0]+args[1])  ## no. need to add . sorry ran out of time. will copy and paste this one and concatenate two strings with an 'and' using {} {} ... and need to actually add. sorru. 
+
+
+
