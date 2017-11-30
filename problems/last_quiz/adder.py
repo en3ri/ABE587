77e56cbe0aeda9a39d53113dfd4132e977be8f14
Irene Liang <iliang@email.arizona.edu>
2017-11-30 14:15:23 -0700

    adding last-quiz again

--- problems/last_quiz/adder.py
+++ problems/last_quiz/adder.py
@@ -6,15 +6,22 @@ import os
 args = sys.argv[1:]
 
 if len(args) != 2:
-        print('Usage: {} arg1 arg2'.format(os.path.basename(sys.argv[0])))
-        sys.exit(1)
+    print('Usage: {} arg1 arg2'.format(os.path.basename(sys.argv[0])))
+    sys.exit(1)
 
 arg1 = args[0]
 arg2 = args[1]
 
-if args[0].isdigit(): 
-    if args[1].isdigit():
-        print(args[0]+args[1])  ## no. need to add . sorry ran out of time. will copy and paste this one and concatenate two strings with an 'and' using {} {} ... and need to actually add. sorru. 
+if arg1.isdigit() and arg2.isdigit():
+    print(int(arg1)+int(arg2))
+
+elif not arg1.isdigit() and not arg2.isdigit():
+    print('{} and {}'.format(args[0], args[1]))
+
+else: 
+    print('Cannot combine number and string')
+    sys.exit(1)
 
 
-
+#Sorry, this is a re-commit. I committed in class as well.
+#I wasn't done because I tried argparse at first.. and I'm typically slow
